2b747708e64f049eceeef7a53905f93f28ab5653
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-06 01:15:20 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,18 +6,17 @@ require 'json'
 class GetRequester
 
   def initialize(url)
-    url = "#{url}"
+    @url = "#{url}"
   end
 
   def get_response_body
-
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
   end
 
   def parse_json
-    programs = JSON.parse(self.get_response_body)
-    programs.collect do |program|
-      program["agency"]
-    end
+    JSON.parse(get_response_body)
   end
 
 end
--- spec/get_requester_spec.rb
+++ spec/get_requester_spec.rb
@@ -9,28 +9,28 @@ describe 'GetRequester' do
     expect { GetRequester.new(URL) }.not_to raise_error
   end
 
-  # describe 'get_response_body' do
-  #   it 'is an instance method on the GetRequester class' do
-  #     get_requester = GetRequester.new(URL)
-  #     expect { get_requester.get_response_body }.not_to raise_error
-  #   end
-  #
-  #   it 'sends a GET request to the URL provided on initialization, returning the body of the request' do
-  #     get_requester = GetRequester.new(URL)
-  #     expect(get_requester.get_response_body).to include(JSON_STRING)
-  #   end
-  # end
-  #
-  # describe 'parse_json' do
-  #   it 'is an instance method on the GetRequester class' do
-  #     get_requester = GetRequester.new(URL)
-  #     expect { get_requester.parse_json }.not_to raise_error
-  #   end
-  #
-  #   it "converts JSON returned from 'get_response_body' into Ruby data structures" do
-  #     get_requester = GetRequester.new(URL)
-  #     results = get_requester.parse_json
-  #     expect(results).to eq(CONVERTED_DATA)
-  #   end
-  # end
+  describe 'get_response_body' do
+    it 'is an instance method on the GetRequester class' do
+      get_requester = GetRequester.new(URL)
+      expect { get_requester.get_response_body }.not_to raise_error
+    end
+
+    it 'sends a GET request to the URL provided on initialization, returning the body of the request' do
+      get_requester = GetRequester.new(URL)
+      expect(get_requester.get_response_body).to include(JSON_STRING)
+    end
+  end
+
+  describe 'parse_json' do
+    it 'is an instance method on the GetRequester class' do
+      get_requester = GetRequester.new(URL)
+      expect { get_requester.parse_json }.not_to raise_error
+    end
+
+    it "converts JSON returned from 'get_response_body' into Ruby data structures" do
+      get_requester = GetRequester.new(URL)
+      results = get_requester.parse_json
+      expect(results).to eq(CONVERTED_DATA)
+    end
+  end
 end
