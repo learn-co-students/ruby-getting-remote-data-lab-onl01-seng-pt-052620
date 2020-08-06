e46baff591b837bc18072ca8cca8fe69bb599e33
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-06 01:13:57 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+
+class GetRequester
+
+  def initialize(url)
+    url = "#{url}"
+  end
+
+  def get_response_body
+
+  end
+
+  def parse_json
+    programs = JSON.parse(self.get_response_body)
+    programs.collect do |program|
+      program["agency"]
+    end
+  end
+
+end
